bca596e318fcfe4ee56aeac9ebeea3213c9926d2
Aldi Andalan <andalanaldi@gmail.com>
2023-10-14 06:16:07 +0700

    chore: reorganize folders

--- lecture-version-refactor/middleware/helmet.js
+++
@@ -1,6 +0,0 @@
-const helmet = require('helmet');
-
-module.exports = (app) => {
-  // Use Helmet to set various security headers
-  app.use(helmet());
-}
--- lecture-version-refactor/middleware/morgan.js
+++
@@ -1,6 +0,0 @@
-const morgan = require('morgan');
-
-module.exports = function(app) {
-  // Use Morgan for logging HTTP requests
-  app.use(morgan('combined'));
-}

191d7a54b4fb9b11f90ae3a79710c2bf372a340a
Aldi Andalan <andalanaldi@gmail.com>
2023-10-14 05:09:01 +0700

    feat : add refresh token, reset password & logout in auth-router

--- week10-version-refactor/routes/auth-route.js
+++ week10-version-refactor/routes/auth-route.js
@@ -1,9 +1,13 @@
 const { Router } = require('express')
-const { register, login } = require('../service/auth-service.js')
+const { register, login, refreshAccessToken, requestResetPassword, resetPassword, logout } = require('../service/auth-service.js')
 
 const authRouter = Router()
 
 authRouter.post('/register', register)
 authRouter.post('/login', login)
+authRouter.post("/refresh-token", refreshAccessToken)
+authRouter.post("/reset-password/request", requestResetPassword)
+authRouter.post("/reset-password", resetPassword)
+authRouter.post("/logout", logout)
 
 module.exports = authRouter
